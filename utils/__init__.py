5bfd03239c0eb8550b91fe6bdb1f77f6a9c0ea73
Amruth22 <89969774+Amruth22@users.noreply.github.com>
2025-10-07 17:29:12 +0530

    Add GeminiClient to utils exports

--- utils/__init__.py
+++ utils/__init__.py
@@ -6,5 +6,6 @@ Utility functions and helpers
 
 from .logging_utils import setup_logging, get_logger
 from .email_notifier import EmailNotifier
+from .gemini_client import GeminiClient
 
-__all__ = ['setup_logging', 'get_logger', 'EmailNotifier']
+__all__ = ['setup_logging', 'get_logger', 'EmailNotifier', 'GeminiClient']
